fdd623bbe4c5f97455c0a264bc8020117f9848dc
juanv <juanv@DESKTOP-TT7GUJN>
2023-04-09 19:21:10 -0400

    Seventh commit

---
+++ src/day2hwproject3/D2HWP3.java
@@ -0,0 +1,10 @@
+package day2hwproject3;
+
+public class D2HWP3 {
+	public static void main(String[] args) {
+		System.out.println("seventh stash create a new local repository");
+		
+		System.out.println("create a remote repository");
+	}
+
+}
